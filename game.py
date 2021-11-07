f6425d745efec25d205b014cd11ef37d216d1ecc
QuangDuc-HUST <ducquangng.03@gmail.com>
2021-11-07 15:27:51 +0700

    Initialize

---
+++ game.py
@@ -0,0 +1,29 @@
+# import algo, main
+from main import *
+
+
+
+# main
+space, ship = environment_initialize(10,7,6)
+print(space.figure)
+print('-+-+'*20)
+i = 0
+while True:
+	i +=1 
+	print(f'Step {i}: Do ', end='')
+
+
+	environment_changes(space=space, step=i)
+
+	# call or do action to Agent 
+	n = input()
+	ship.move(n)
+	# end action
+
+	if check_collision(space=space):
+		break
+	if check_winning(space=space):
+		print('Winning')
+		break
+	
+	space.show()
