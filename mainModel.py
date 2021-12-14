cfce13e0ef0f4baccbd242f388b538ecf36505b6
Thanh Nguyen <npthanh132@gmail.com>
2021-12-14 11:35:54 +0700

    add visualize to mainModel

--- mainModel.py
+++ mainModel.py
@@ -1,7 +1,8 @@
+from visualize_mode import display
 from Model import GameModel
 from LL import online  # Sourcecode: LL
 from HD import local_search  # Sourcecode: HD
-from QD import miniMax ,testAsearch  # Sourcecode: QD
+from QD import miniMax, testAsearch  # Sourcecode: QD
 
 
 # Just be normal game, input
@@ -13,15 +14,15 @@ def main():
     game = GameModel()
     game.initialize(height=10, width=7, num=14)
 
-    ### One time
+    # One time
 
     game.run(local_search)
+
     # game.run(online)
     # game.run(getinput)
-    
+
     # print(game.getStatesStatistic())
     # print(game.getActionsStatistic())
-
 
     # game.run(testinputOffline)
     # game.run(testAsearch)
@@ -30,11 +31,11 @@ def main():
     # print(game.getActionsStatistic())
     # print(len(game.getActionsStatistic()))
 
-
     # __SAVE DATA__
     game.saveData('Test1')
+    display(game.getStatesStatistic())
 
-    ## Evaluatate multiple time
+    # Evaluatate multiple time
 
     # eva = game.getEvaluate()
 
@@ -43,6 +44,6 @@ def main():
     # eva.saveGame('Testmulti')
     # eva.evamultitime(online, True, 10)
 
-   
+
 if __name__ == '__main__':
     main()
